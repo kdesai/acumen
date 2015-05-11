4f877c0ccd936eebb3b101a0a6d1176bedffcd4f
Nedfex <nedfex@gmail.com>
2015-05-11 14:51:44 -0400

    latest version

--- chart_links.php
+++ chart_links.php
@@ -2,6 +2,7 @@
 set_time_limit(6000000); 
 //$link = mysql_connect('140.112.233.123', 'root', 'lapy110');
 $link = mysql_connect('127.0.0.1', 'websysS15GB4', 'websysS15GB4!!');
+//$link = mysql_connect('127.0.0.1', 'root', '');
 mysql_query("SET NAMES utf8");
 mysql_select_db("websysS15GB4",$link);
 
@@ -25,6 +26,19 @@ $data = array();
 $data[0] = array();
 $data[1] = array($TICKER);
 $data[2] = array();
+$data[3] = array();
+$otc = 0;
+
+$query = "SELECT * FROM  `testcount2` WHERE MAJOR_TICKER = '$TICKER';";
+$result = mysql_query($query,$link);
+$row2 = mysql_fetch_array($result);
+$OTHER_TICKERS = json_decode($row2['OTHER_TICKERS_JSON'],true);
+//print_r($OTHER_TICKERS);
+
+foreach ($OTHER_TICKERS as $k => $v) {
+    $data[0][$array_counter++] = array("source" => $TICKER, "target" => $k,"type" => "licensing");
+    $data[3][$otc++] = $k;
+}
 
 
 for($i=1;$i<=9;$i++)
@@ -46,6 +60,22 @@ for($i=1;$i<=9;$i++)
 		 $data[0][$array_counter++] = array("source" => $RELATED_TICKERS[$i]['symbol'], "target" => $RELATED_TICKERS_2[$j]['symbol'],"type" => "licensing");
 		 
 	}
+
+	$query = "SELECT * FROM  `testcount2` WHERE MAJOR_TICKER = '".$RELATED_TICKERS[$i]['symbol']."';";
+	$result = mysql_query($query,$link);
+
+	if(mysql_num_rows($result)>0)
+	{
+		//echo "$i  ".$RELATED_TICKERS[$i]['symbol'].mysql_num_rows($result);
+		$row2 = mysql_fetch_array($result);
+		$OTHER_TICKERS = json_decode($row2['OTHER_TICKERS_JSON'],true);
+
+		foreach ($OTHER_TICKERS as $k => $v) {
+		    $data[0][$array_counter++] = array("source" => $RELATED_TICKERS[$i]['symbol'], "target" => $k,"type" => "licensing");
+		    $data[3][$otc++] = $k;
+		}
+	}
+
 }
 echo json_encode($data);
 ?>
